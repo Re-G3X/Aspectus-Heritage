2bc46868c46940b54c30a561253487adf888f974
Alisson dos Santos <alisson_s_pereira@outlook.com>
2024-06-29 19:39:26 -0300

    Funcionando

--- Assets/Scenes/Scripts/Game/LevelGenerator/PostProcessing/PostProcessingController.cs
+++ Assets/Scenes/Scripts/Game/LevelGenerator/PostProcessing/PostProcessingController.cs
@@ -1,4 +1,8 @@
 using SOHNE.Accessibility.Colorblindness;
+<<<<<<< Updated upstream
+=======
+using UnityEngine.UI;
+>>>>>>> Stashed changes
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
@@ -6,6 +10,10 @@ using UnityEngine.Rendering.Universal;
 public class PostProcessingController : MonoBehaviour {
 
     public Material material;
+<<<<<<< Updated upstream
+=======
+    public Text text;
+>>>>>>> Stashed changes
 
     void Start() {
         // Obter o componente de volume
@@ -16,37 +24,73 @@ public class PostProcessingController : MonoBehaviour {
 
         setShaderNull();
 
+<<<<<<< Updated upstream
         if (randomValue == 1) {//Glaucoma
             setVignette(); 
             Debug.Log("Chamando Glaucoma");
         }
         if (randomValue == 2) {//Catarata
+=======
+
+        if (randomValue == 1) {//Glaucoma
+            setVignette();
+            text.text = "Glaucoma";
+            Debug.Log("Chamando Glaucoma");
+        }
+        if (randomValue == 2) {//Catarata
+            text.text = "Catarata";
+>>>>>>> Stashed changes
             setBloom(volume);
             Debug.Log("Chamando catarata");
         }
         if (randomValue == 3) {//Degeneração Macular 
+<<<<<<< Updated upstream
             setDegeneracaoMacular();            
+=======
+            setDegeneracaoMacular();
+            text.text = "Degeneração Macular";
+>>>>>>> Stashed changes
             Debug.Log("Chamando Degeneração macular");
         }
         if (randomValue == 4) {//Protanopia                
             Colorblindness.Instance.Change(1);
+<<<<<<< Updated upstream
+=======
+            text.text = "Protanopia";
+>>>>>>> Stashed changes
             Debug.Log("Chamando Protanopia");
         }
         if (randomValue == 5) {//Deuteranopia
             Colorblindness.Instance.Change(3);
+<<<<<<< Updated upstream
+=======
+            text.text = "Deuteranopia";
+>>>>>>> Stashed changes
             Debug.Log("Chamando Deuteranopia");
         }
         if (randomValue == 6) {//Tritanopia
             Colorblindness.Instance.Change(5);
+<<<<<<< Updated upstream
+=======
+            text.text = "Tritanopia";
+>>>>>>> Stashed changes
             Debug.Log("Chamando Tritanopia");
         }
         if (randomValue == 7) {//Acromatopsia
             Colorblindness.Instance.Change(7);
+<<<<<<< Updated upstream
+=======
+            text.text = "Acromatopsia";
+>>>>>>> Stashed changes
             Debug.Log("Chamando Acromatopsia");
         }
     }    
 
+<<<<<<< Updated upstream
     private void setVignette() {        
+=======
+    private void setVignette() {
+>>>>>>> Stashed changes
         material.SetInt("_IsEnabled", 1);
         material.SetInt("_MinusOne", 0);
         material.SetFloat("_CutoffSize", 0.2f);
@@ -55,6 +99,10 @@ public class PostProcessingController : MonoBehaviour {
     private void setDegeneracaoMacular() {
         material.SetInt("_IsEnabled", 1);
         material.SetInt("_MinusOne", 1);
+<<<<<<< Updated upstream
+=======
+        material.SetFloat("_CutoffSize", 0.01f);
+>>>>>>> Stashed changes
     }
 
     private void setShaderNull() {
